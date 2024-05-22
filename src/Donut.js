82a04ba255f3d8955adbc73bec68f73534fd6a50
Samuel Jeyachandran <david.jeyachandran@sim.org>
2024-05-22 21:15:32 +1000

    Fixed bug of buying cofee link

--- src/Donut.js
+++ src/Donut.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Donut = () => {
     return (
         <div>
-            <a href="https://www.buymeacoffee.com/samuel18jec" target="_blank">
+            <a href="https://www.buymeacoffee.com/samueljey" target="_blank">
                 <img
                     src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Donut"
                     style={{height: '60px',width: '217px'}} />
